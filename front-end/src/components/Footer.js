107719677aa01f80a30d4f6453f551daf40e275d
Mayur-Thinkbridge <mayurshinde6776@gmail.com>
2024-01-08 14:01:49 +0530

    footer text style

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='container-fluid  text-light p-2 text-center footer footer-bottom' style={{background:'skyblue'}}>
-      <h2>E-Commerce</h2>
+      <p>E-Commerce</p>
     </div>
   );
 };
